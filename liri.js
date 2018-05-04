fe4753df37120afd0eaa9109622c729b8fab5269
Baran Birol <baranbirol@barans-mbp.lan>
2018-05-03 19:03:57 -0700

    twitter is done

--- liri.js
+++ liri.js
@@ -16,16 +16,24 @@ var b = process.argv[3];
 //Twitter machine
 //make it so that that my-tweets is [2]
 //need to also think for spotify later on, becasue that is going to use [2] as well
-function tweets() {
+switch(a) {
 
-    var params = { count: 20};
+    case 'my-tweets':
 
-    client.get("statuses/user_timeline", params, function(error, tweets, response) {
-        if (!error){
-            throw error;
+    var client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+      });
 
-    }
+      var params = {screen_name: 'NigelSchumacher', count: 20};
+      client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            for (let i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+            };
+        };
+      });
 
-}
-}
-    
+    };
